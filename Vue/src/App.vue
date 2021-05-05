3a7067b6715ce799fc97f2888c720bf5d6bd0fc4
Akhil Harwadekar <61285173+Akhil-Harwadekar@users.noreply.github.com>
2021-05-05 23:53:52 +0530

    Add files via upload

---
+++ Vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <NavPage msg="MY CONTACT BOOK"/>
+</template>
+
+<script>
+import NavPage from './components/NavPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
